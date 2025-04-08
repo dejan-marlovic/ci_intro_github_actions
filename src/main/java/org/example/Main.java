1e85bd92aa352234597d0eeb2d6b512385bb1fc1
dejan-marlovic <dmarlovic83@gmail.com>
2025-04-08 11:53:29 +0200

    added gitHub action

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ package org.example;
 1. Skapa en git action
 2. Skapa ett enhetstest lokalt
 3. Ändra gh action så att enhetstest körs på github
+    -klona repo
+    -installera jdk
+    -installera dependencies och kör
  */
 
 public class Main {
