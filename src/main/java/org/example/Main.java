438df380f1ba8411ab967be83703385d33e36368
dejan-marlovic <dmarlovic83@gmail.com>
2025-04-08 11:14:50 +0200

    added gitHub action

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,11 @@
 package org.example;
 
+/*
+1. Skapa en git action
+2. Skapa ett enhetstest lokalt
+3. Ändra gh action så att enhetstest körs på github
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello test, World!");
